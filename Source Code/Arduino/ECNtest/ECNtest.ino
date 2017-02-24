46f4b3abb1eaff638cfea24a33c5fef49637988d
cputratama <chris.dp.41@gmail.com>
2017-02-24 07:11:16 +0700

    Update ECNtest 24-2-2017

--- Source Code/Arduino/ECNtest/ECNtest.ino
+++ Source Code/Arduino/ECNtest/ECNtest.ino
@@ -184,21 +184,10 @@ void setup()
 
 void loop()
 {
-  data = Acc_Read();
-  msg.accelerations[i].x = data.x;
-  msg.accelerations[i].y = data.y;
-  msg.accelerations[i].z = data.z;
+  
   
   // This sketch displays information every time a new sentence is correctly encoded.
-  while (ss.available() > 0)
-    if (gps.encode(ss.read()))
-      displayInfo();
-
-  if (millis() > 5000 && gps.charsProcessed() < 10)
-  {
-    Serial.println(F("No GPS detected: check wiring."));
-    while(true);
-  }
+  
 
   data = Acc_Read();
   msg.accelerations[i].x = data.x;
